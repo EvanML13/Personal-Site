bc550218fa2b6c7188aa785b762aec981c881a70
EvanML13 <evanmleonard06@gmail.com>
2026-09-11 12:25:42 -0400

    Added Scroll Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import { useEffect } from "react";
 import Navbar from "./components/layout/Navbar";
 import Portfolio from "./pages/Portfolio";
 import Reflection from "./pages/Reflection";
@@ -7,6 +8,12 @@ import Reflection from "./pages/Reflection";
 // Handle Routes When Switching Between Pages
 function AnimateRoutes() {
   const location = useLocation()
+
+  // Reset Scroll Position Every Time The Route Changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname])
+
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
